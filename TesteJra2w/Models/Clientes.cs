6de5bfc6ce14a607ad62d0b97adcf4651d33e6d7
study <study@DESKTOP-EAL099F>
2021-09-10 12:43:10 -0300

    Adicionar arquivos de projeto.

---
+++ TesteJra2w/Models/Clientes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteJra2w.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public List<Enderecos> Enderecos { get; set; }
+
+
+    }
+}
---
+++ TesteJra2w/Models/Enderecos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteJra2w.Models
+{
+    public class Enderecos
+    {
+        public int Id { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string cep { get; set; }
+
+        public Clientes Clientes { get; set; }
+    }
+}
